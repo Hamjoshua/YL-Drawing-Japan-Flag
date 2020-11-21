8204a0e80415c7dc5d016b6ee643d8e03cc34c60
Hamjoshua <72654119+Hamjoshua@users.noreply.github.com>
2020-11-21 10:01:01 +0700

    + add random color + change bug with paintEvent

--- Yellow_rounds.py
+++ Yellow_rounds.py
@@ -1,32 +1,66 @@
 import sys
-from PyQt5 import uic, QtGui
+from PyQt5 import uic, QtGui, QtCore, QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from random import choices, choice
+from random import choices
 
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
 
 
-class MainWindow(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(640, 480)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(120, 310, 371, 81))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Жми на кнопку!"))
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
+        self.setupUi(self)
         self.setWindowTitle('Simulator of Japan flag')
-        self.pushButton.clicked.connect(self.repaint)
+        self.pushButton.pressed.connect(self.draw)
+        self.flag = False
 
-    def draw(self, qp):
-        qp.begin(self)
-        qp.setBrush(QtGui.QBrush(QtGui.QColor('#FF0000')))
-        qp.drawEllipse(self.rect().center(), *self.random_size())
+    def draw(self):
+        self.flag = True
+        self.repaint()
 
     def paintEvent(self, event):
-        qp = QtGui.QPainter()
-        self.draw(qp)
-        qp.end()
+        if self.flag:
+            qp = QtGui.QPainter(self)
+            qp.begin(self)
+            qp.setBrush(QtGui.QBrush(self.random_color()))
+            qp.drawEllipse(self.rect().center(), *self.random_size())
+            qp.end()
+            self.flag = False
         
     def random_size(self):
         return choices(range(256), k=2)
+
+    def random_color(self):
+        return QtGui.QColor(*choices(range(256), k=3))
 
 
 if __name__ == '__main__':
